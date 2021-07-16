52e758c90a28d9a38bd3d3912980714279d5d743
Jan Betley <jan.betley@golem.network>
2021-07-16 14:43:17 +0200

    load balancing works

--- ya_httpx_client/session.py
+++ ya_httpx_client/session.py
@@ -17,12 +17,12 @@ class Cluster:
        should be merged into the same cluster-wrapper-thingy, but we can't do this without yapapi-side modifications.
        Currently I don't think `yapapi.Cluster` could be used instead of this one.
     '''
-    def __init__(self, manager, image_hash, start_steps, cnt):
+    def __init__(self, manager, image_hash, start_steps):
         self.manager = manager
         self.image_hash = image_hash
         self.start_steps = start_steps
 
-        self.expected_cnt = cnt
+        self.expected_cnt = 0
 
         self.request_queue = asyncio.Queue()
         self.cls = self._create_cls()
@@ -45,16 +45,18 @@ class Cluster:
         if self.new_services_starter_task is not None:
             self.new_services_starter_task.cancel()
 
-    def resize(self, new_cnt):
-        self.expected_cnt = new_cnt
+    def set_size(self, size):
+        if isinstance(size, int):
+            self.expected_cnt = size
+        else:
+            self.expected_cnt = size(self)
 
     async def _start_new_services(self):
         while True:
-            new_tasks = [asyncio.create_task(self._manage()) for _ in range(self.cnt, self.expected_cnt)]
-            if new_tasks:
-                print(f"CREATED {len(new_tasks)} NEW TASKS")
+            expected_cnt = int(self.expected_cnt)
+            new_tasks = [asyncio.create_task(self._manage()) for _ in range(self.cnt, expected_cnt)]
             self.manager_tasks += new_tasks
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(1)
 
     async def _manage(self):
         service_wrapper = None
@@ -63,8 +65,7 @@ class Cluster:
             if service_wrapper is None:
                 service_wrapper = self.manager.create_service(self.cls)
 
-            if self.expected_cnt < self.cnt:
-                print("STOPPING BECAUSE THERE IS TOO MUCH OF US")
+            if int(self.expected_cnt) < self.cnt:
                 service_wrapper.stop()
                 break
 
@@ -111,15 +112,16 @@ class Session:
         self.manager = ServiceManager(executor_cfg)
         self.clusters = {}
 
-    def resize(self, url, cnt):
-        self.clusters[url].resize(cnt)
+    def set_size(self, url, size):
+        self.clusters[url].set_size(size)
 
     def startup(self, url, image_hash, service_cnt=1):
         if url in self.clusters:
             raise KeyError(f'Service for url {url} already exists')
 
         def define_service(start_steps):
-            self.clusters[url] = Cluster(self.manager, image_hash, start_steps, service_cnt)
+            self.clusters[url] = Cluster(self.manager, image_hash, start_steps)
+            self.set_size(url, service_cnt)
 
         return define_service
 
