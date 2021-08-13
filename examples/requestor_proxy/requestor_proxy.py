fb6973bc58fb3eea0daa3ff04b607e7643a12815
Jan Betley <jan.betley@golem.network>
2021-08-13 14:27:45 +0200

    readability and comments in requestor_proxy

--- examples/requestor_proxy/requestor_proxy.py
+++ examples/requestor_proxy/requestor_proxy.py
@@ -3,25 +3,33 @@ from quart import Quart
 from ya_httpx_client.serializable_request import Request, Response
 from ya_httpx_client.session import Session
 
+#   This is the only "host" we create here, so the name could be exactly anything
+PROVIDER_URL = 'http://provider_http_server'
 
-HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
-PROVIDER_URL = 'http://calc'
+#   Image hash and the `startup` function define the HTTP server that will be running on the provider,
+#   and thus the final behaviour of our requestor proxy.
+#   Here we use the same image/startup as in the other example (calculator), but any server that would work
+#   in a Dockerfile should be fine.
 IMAGE_HASH = '1f43e06ecc4ef40084efcf57131aa6056c57b5732bef2bcb6a8cdad2'
-INIT_CLUSTER_SIZE = 1
-
-executor_cfg = {'budget': 10, 'subnet_tag': 'devnet-beta.2'}
-
-app = Quart(__name__)
-
-
 def startup(ctx, listen_on):
     ctx.run("/usr/local/bin/gunicorn", "--chdir", "/golem/run", "-b", listen_on, "calculator_server:app", "--daemon")
 
+#   Number of providers that will be processing requests. NOTE: providers have no shared state, so
+#   numbers >1 make sense only for stateless servers.
+INIT_CLUSTER_SIZE = 1
+
+#   Golem configuration - this will be passed directly to the Golem object
+#   https://handbook.golem.network/yapapi/api-reference#golem-objects
+EXECUTOR_CFG = {'budget': 10, 'subnet_tag': 'devnet-beta.2'}
+
 
 async def init_session():
-    #   NOTE: it is important to initialize the session in the quart-managed async context,
-    #         so we have everything running in the same loop
-    session = Session(executor_cfg)
+    '''
+    Create a Session object.
+    NOTE: it is important to initialize the session in the quart-managed async context,
+          because we need everything to run in the same loop
+    '''
+    session = Session(EXECUTOR_CFG)
     session.startup(
         url=PROVIDER_URL,
         image_hash=IMAGE_HASH,
@@ -30,21 +38,27 @@ async def init_session():
     return session
 
 
+async def forward_request():
+    '''
+    Quart request -> provider-based HTTP server -> Quart response
+    '''
+    req = await Request.from_quart_request()
+    req.replace_mount_url(PROVIDER_URL)
+    res = await app.yhc_client.send(req.as_httpx_request())
+    return Response.from_httpx_response(res).as_quart_response()
+
+
+app = Quart(__name__)
+
 @app.while_serving
-async def start_provider():
+async def with_yhc_session():
     session = await init_session()
     async with session.client() as client:
         app.yhc_client = client
         yield
     await session.close()
 
-
-async def forward_request():
-    req = await Request.from_quart_request()
-    req.replace_mount_url(PROVIDER_URL)
-    res = await app.yhc_client.send(req.as_httpx_request())
-    return Response.from_httpx_response(res).as_quart_response()
-
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:_path>', methods=HTTP_METHODS)
