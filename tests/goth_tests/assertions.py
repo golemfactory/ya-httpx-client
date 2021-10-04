2cc5fb31b43a3737cbe966d865c4adbb46163457
Jan Betley <jan.betley@golem.network>
2021-10-04 13:43:45 +0200

    goth tests removed (because they are not ready yet)

--- tests/goth_tests/assertions.py
+++
@@ -1,8 +0,0 @@
-from goth.assertions import EventStream
-
-
-async def assert_no_errors(output_lines: EventStream[str]):
-    """Assert that no output line contains the substring `ERROR`."""
-    async for line in output_lines:
-        if "ERROR" in line:
-            raise AssertionError("Command reported ERROR")
